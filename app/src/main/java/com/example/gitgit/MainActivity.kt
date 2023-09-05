74a7196659203dcb47be6803ca33dd48dc6bdcd3
Jorge Flores <flores.jorgeluis@gmail.com>
2023-09-04 19:54:25 -0600

    Cambios

--- app/src/main/java/com/example/gitgit/MainActivity.kt
+++ app/src/main/java/com/example/gitgit/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GitgitTheme {
-        Greeting("Android")
+        Greeting("Hello Android")
     }
 }
